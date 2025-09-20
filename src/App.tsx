26ac52dfb1be12f7672da9f885245829b66ca6e2
John <kimo87@gmail.com>
2025-09-20 18:35:48 +0900

    [FE] 리액트 - 8강: useEffect란 무엇인가? 下

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,30 @@
 import { useEffect, useState } from "react";
 
-function App() {
-
-  //useEffect는 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 훅(Hook)입니다.
-
-
-  const[name,setName] = useState<string>("");
-  const[nickname,setNickname] =useState<string>("");
-
+function Timer(){
+  const [count,setCount] = useState<number>(0);
+  
   useEffect(()=>{
-    // 해당 컴포넌트가 최초 렌덩링이 될 때 , useEffect가 실행이 되고,
-    //우리가 선언한 state 즉, 상태값이 변화되더라도 useEffect가 실행되는 것을 보아
-    //state 즉 , 상태 값이 변화되며 해당 컴포넌트는 재렌더링이 된다라는것을 알수가 있습니다.
-    console.log("컴포넌트가 렌더링 될 때마다 특정 작업을 수행합니다");
-    console.log("name: ",name);
-    console.log("nickname",nickname);
-  })
+    const id =setInterval(()=>{
+      console.log("Interval 실행됨");
+      setCount((c)=>c+1);
+    },1000);
 
-    useEffect(()=>{
-    console.log("마운트가 될때만 수행합니다. - 최초 1회 실시");
-    console.log("name: ",name);
-    console.log("nickname",nickname);
-  },[])
-    useEffect(()=>{
-    console.log("name이라는 상태값이 변할 경우에만 수행합니다.");
-    console.log("name: ",name);
-    console.log("nickname",nickname);
-  },[name])
-
-
-   const onchangeName = (event : React.ChangeEvent<HTMLInputElement>)=>setName(event.target.value);
-
-
-   const onchangeNickName = (event : React.ChangeEvent<HTMLInputElement>)=>setNickname(event.target.value);
-   
-
-  return <div>
-    <input type="text" value={name} onChange={onchangeName}/>
-    <input type="text" value={nickname} onChange={onchangeNickName}/>
-    <div>
-      <b>이름 : {name}</b>
-      <b>별명 : {nickname}</b>
-    </div>
-  </div>;
+    return()=>{
+      console.log(" cleanup: 이전 타이머 제거됨");
+      clearInterval(id);
+    }
+  },[]);
+  return <div>카운트 : {count}</div>
 }
 
-export default App
+export default function App(){
+ const [visible,setVisible] = useState<boolean>(true);
+
+ return (
+  <div>
+    {visible&&<Timer/>}
+    <button onClick={()=>setVisible(!visible)}>{visible?"숨기기" :"보이기"}</button>
+  </div>
+ )
+
+}
