6f89366de440e653edbc16d5e3c11ca78231081d
John <kimo87@gmail.com>
2025-09-27 18:11:55 +0900

    13장 변경
    - 13장 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useRef } from "react";
+import { AppFooter, AppHeader } from "./components/common";
+import { ThemeProvider } from "./components/theme-provider";
+function App(){
 
-function App() {
-    const inputElement = useRef<HTMLInputElement | null>(null);
-    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-    const handleClick = () => {
-        // useRef 동작
-        inputElement.current?.focus();
-        fileInputRef.current?.click();
-    };
 
     return (
-        <div>
-            <input type="text" ref={inputElement} />
-            <input type="file" ref={fileInputRef} />
-            <button onClick={handleClick}>등록</button>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="page">
+        <AppHeader/>
+        <div className="container"></div>
+        <AppFooter/>
         </div>
+        </ThemeProvider>
     );
 }
 
